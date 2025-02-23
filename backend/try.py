60d03c4b5c914cf05935f7a6cc2b1e33479e258b
srlog <146011589+srlog@users.noreply.github.com>
2025-02-23 13:12:03 +0530

    Flask backend to handle api requests and react frontend..

---
+++ backend/try.py
@@ -0,0 +1,18 @@
+
+import os
+from PyPDF2 import PdfReader
+
+
+
+def extract_text_from_pdf(file_obj):
+    reader = PdfReader(file_obj)
+    text = ''
+    for page in reader.pages:
+        text += page.extract_text() or ""
+    return text
+
+pdf = open('vce.pdf', 'rb')
+text = extract_text_from_pdf(pdf)
+print(text)
+
+
